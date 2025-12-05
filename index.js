937b7c03fece10ff63452bfe2f724ec76df8b921
Nibir-Ahmed <nibir7864@gmail.com>
2025-12-06 00:46:53 +0600

    adding server to client

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
@@ -6,6 +7,29 @@ const cors = require ('cors');
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//i7mB01Ic57scuwT4
+//learnhub_user
+
+const uri = "mongodb+srv://learnhub_user:i7mB01Ic57scuwT4@cluster0.oyr6l0t.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('simple crud server is running ')
